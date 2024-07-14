345582ea712b0b92ef671994fc314b769f7d4b0c
Ashishkhose7 <khoseashish7@gmail.com>
2024-07-14 21:11:49 +0530

    popovers

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,22 +1,84 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import Popover from '@mui/material/Popover';
 
 const Dashboard = () => {
-    useEffect(() => {
+    // useEffect(() => {
 
-        console.log("dashboard loaded")
-    }, [])
+    //     console.log("dashboard loaded")
+    // }, [])
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [anchorE2, setAnchorE2] = useState(null);
+    const [anchorE3, setAnchorE3] = useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const handleClick2 = (event) => {
+        setAnchorE2(event.currentTarget);
+    };
+
+    const handleClose2 = () => {
+        setAnchorE2(null);
+    };
+
+    const handleClick3 = (event) => {
+        setAnchorE3(event.currentTarget);
+    };
+
+    const handleClose3 = () => {
+        setAnchorE3(null);
+    };
+  
+    const open = Boolean(anchorEl);
+    const open2 = Boolean(anchorE2);
+    const open3 = Boolean(anchorE3);
+    const id = open ? 'simple-popover' : undefined;
+    const id2 = open2 ? 'simple-popover' : undefined;
+    const id3 = open3 ? 'simple-popover' : undefined;
 
     return (
         <>
             <section className="mainApp d-flex">
                 <div className="leftPanel">
                     <header>
-                        <button className="trigger">
+                        <button className="trigger" aria-describedby={id3} variant="contained" onClick={handleClick3}>
                             <svg viewBox="0 0 24 24">
                                 <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
                             </svg>
                         </button>
+                        <Popover 
+                            id={id3}
+                            open={open3}
+                            anchorE3={anchorE3}
+                            onClose={handleClose3}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            >
+                        <ul className="p-1 px-2 popoverlist">
+                            <p> <i className="material-icons mute">bookmark</i>&nbsp;&nbsp;&nbsp;Saved Messages</p>
+                            <p> <i className="material-icons call">menu_book</i>&nbsp;&nbsp;&nbsp;My Stories</p>
+                            <p> <i className="material-icons call">contacts</i>&nbsp;&nbsp;&nbsp;Contacts</p>
+                            <p> <i className="material-icons call">settings</i>&nbsp;&nbsp;&nbsp;Settings</p>
+                            <p> <i className="material-icons call">dark_mode</i>&nbsp;&nbsp;&nbsp;Dark Mode</p>
+                            <p> <i className="material-icons call">animation</i>&nbsp;&nbsp;&nbsp;Animations</p>
+                            <p> <i className="material-icons call">info</i>&nbsp;&nbsp;&nbsp;Telegram Features</p>
+                            <p className=""> <i className="material-icons call">bug_report</i>&nbsp;&nbsp;&nbsp;Report Bug</p>
+                            <p className=""> <i className="material-icons call">text_format</i>&nbsp;&nbsp;&nbsp;Switch to A version</p>
+                            <p className=""> <i className="material-icons call">download</i>&nbsp;&nbsp;&nbsp;Install App</p>
 
+                        </ul>
+                        </Popover>
                         <input className="searchChats" type="search" placeholder="Search..." />
                     </header>
                     <div className="chats px-2">
@@ -76,11 +138,38 @@ const Dashboard = () => {
                             <button className="tbButton search">
                                 <i className="material-icons">&#xE8B6;</i>
                             </button>
-                            <button className="tbButton otherOptions">
-                                <i className="material-icons">more_vert</i>
+                            <button className="tbButton otherOptions" aria-describedby={id} variant="contained" onClick={handleClick}>
+                                <i className="material-icons" >more_vert</i>
                             </button>
                         </div>
-
+                                    {/* popover */}
+                        <Popover 
+                            id={id}
+                            open={open}
+                            anchorEl={anchorEl}
+                            // anchorReference="anchorPosition"
+                            onClose={handleClose}
+                            // anchorPosition={{ top: 380, left: 1300 }}
+                            anchorOrigin={{
+                                vertical: 'center',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right',
+                            }}
+                            >
+                        <ul className="p-1 px-2 popoverlist">
+                            <p> <i className="material-icons mute">volume_off</i>&nbsp;&nbsp;&nbsp;Mute</p>
+                            <p> <i className="material-icons call">phone_in_talk</i>&nbsp;&nbsp;&nbsp;Call</p>
+                            <p> <i className="material-icons call">video_call</i>&nbsp;&nbsp;&nbsp;Video Call</p>
+                            <p> <i className="material-icons call">check_circle</i>&nbsp;&nbsp;&nbsp;Select Messages</p>
+                            <p> <i className="material-icons call">share</i>&nbsp;&nbsp;&nbsp;Share Contact</p>
+                            <p> <i className="material-icons call">redeem</i>&nbsp;&nbsp;&nbsp;Gift Premium</p>
+                            <p> <i className="material-icons call">block</i>&nbsp;&nbsp;&nbsp;Block User</p>
+                            <p className="text-danger"> <i className="material-icons call">delete</i>&nbsp;&nbsp;&nbsp;Delete Chat</p>
+                        </ul>
+                        </Popover>
                         <button className="go-back text-light fw-bold">
                             RM
                             {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Go back</title><path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" /></svg> */}
@@ -136,9 +225,39 @@ const Dashboard = () => {
 
                     <div className="replyBar d-flex mt-3">
                         <div className="text-in mt-1">
-                        <button className="attach ps-2">
-                            <i className="material-icons">face</i>
+                        <button className="attach ps-2" aria-describedby={id2} variant="contained" onClick={handleClick2}>
+                            <i className="material-icons">mood</i>
                         </button>
+                        <Popover
+                            id={id2}
+                            open={open2} 
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                              }}
+                              transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                              }}
+                            onClose={handleClose2}
+                        >
+                        <div className="p-1 px-2 popoverlist">
+                            <li>
+                                <i className="material-icons mute">face</i>
+                                <i className="material-icons call">mood</i>
+                                <i className="material-icons call">add_reaction</i>
+                                <i className="material-icons call">face_6</i>
+                                <i className="material-icons call">face_3</i>
+                            </li>
+                            <li>
+                                <i className="material-icons call">mood_bad</i>
+                                <i className="material-icons call">face_4</i>
+                                <i className="material-icons call">face_2</i>
+                                <i className="material-icons call">face_5</i>
+                                <i className="material-icons call">flag</i>
+                            </li>
+                        </div>
+                        </Popover>
                         <input type="text" className="replyMessage pe-5" placeholder="Type your message..." />
                         <button className="attach pe-3">
                             <i className="material-icons d45">attach_file</i>
